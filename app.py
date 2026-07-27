9e4813189aa0ad77cfa055713e7e39355e5651ef
BushOnBush <64505874+BushOnBush@users.noreply.github.com>
2026-07-27 10:07:27 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -424,8 +424,8 @@ def load_model():
     model.fc = nn.Linear(model.fc.in_features, 2)
 
     model_path = hf_hub_download(
-        repo_id="BushOnBush/aiimagedetector",
-        filename="best_model.pth"
+        repo_id="BushOnBush/aiimagedetector2",
+        filename="best_model2.pth"
     )
 
     model.load_state_dict(torch.load(model_path, map_location=device))
